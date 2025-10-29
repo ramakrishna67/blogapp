f82a6491026c89aa1a54db82cf9acc283828f6e6
ramakrishna67 <ramakrishnapaila64@gmail.com>
2025-10-29 20:28:21 +0530

    update in ssl mode

--- lib/db/drizzle.ts
+++ lib/db/drizzle.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 const client = postgres(process.env.DATABASE_URL!, {
-  ssl: "require",
+  ssl: { rejectUnauthorized: false },
 });
 export const db = drizzle(client);
 (async () => {
