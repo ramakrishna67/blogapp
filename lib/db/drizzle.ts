4ea92f69ecd5bf99e2d6adaea77df430696becf5
ramakrishna67 <ramakrishnapaila64@gmail.com>
2025-10-29 19:09:09 +0530

    Cleared production conflicts

--- lib/db/drizzle.ts
+++ lib/db/drizzle.ts
@@ -1,7 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const client = postgres(process.env.DATABASE_URL!);
+const client = postgres(process.env.DATABASE_URL!, {
+  ssl: "require",
+});
 export const db = drizzle(client);
 (async () => {
   try {
